3c67c8fb2cf61e0a243b918cb65e31908e231131
Ariel Davis <ariel.z.davis@icloud.com>
2021-08-03 17:51:22 -0700

    Start

---
+++ crates/statics/src/lib.rs
@@ -0,0 +1,9 @@
+//! Bidirectional typechecking for arbitrary-rank types.
+//!
+//! Mostly adapted from [this MSR paper][1].
+//!
+//! [1]: https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/putting.pdf
+
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(rust_2018_idioms)]
---
+++ readme.md
@@ -0,0 +1,8 @@
+# BiDir
+
+Bidirectional typechecking for arbitrary-rank types.
+
+Mostly adapted from [this MSR paper][1].
+
+[1]:
+  https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/putting.pdf
