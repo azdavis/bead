397a9ec70e821c959c26fd2eeab95a3256afadcc
Ariel Davis <ariel.z.davis@icloud.com>
2021-08-08 19:11:26 -0700

    Make some defs pub(crate)

--- crates/statics/src/defs.rs
+++ crates/statics/src/defs.rs
@@ -5,11 +5,11 @@ use uniq::{Uniq, UniqGen};
 
 /// A name in code, aka a variable, an identifier.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Name(Uniq);
+pub(crate) struct Name(Uniq);
 
 impl Name {
   /// Returns a new [`Name`].
-  pub fn new(u: Uniq) -> Self {
+  pub(crate) fn new(u: Uniq) -> Self {
     Self(u)
   }
 }
@@ -36,13 +36,13 @@ pub enum Ty {
 
 impl Ty {
   /// Returns a function type from `arg` to `res`.
-  pub fn fun(arg: Self, res: Self) -> Self {
+  pub(crate) fn fun(arg: Self, res: Self) -> Self {
     Self::Fun(Box::new(arg), Box::new(res))
   }
 
   /// Returns a forall type, unless `tvs` is empty, in which case this returns
   /// the [`Ty`] inside `rho`.
-  pub fn for_all(tvs: Vec<BoundTyVar>, rho: Rho) -> Self {
+  pub(crate) fn for_all(tvs: Vec<BoundTyVar>, rho: Rho) -> Self {
     if tvs.is_empty() {
       rho.into_inner()
     } else {
@@ -59,7 +59,7 @@ pub struct Rho(Ty);
 
 impl Rho {
   /// Returns a new [`Rho`]. Panics in debug mode if it is a [`Ty::ForAll`].
-  pub fn new(ty: Ty) -> Self {
+  pub(crate) fn new(ty: Ty) -> Self {
     debug_assert!(Self::is_valid(&ty));
     Self(ty)
   }
@@ -69,7 +69,7 @@ impl Rho {
   }
 
   /// Unwraps the [`Ty`].
-  pub fn into_inner(self) -> Ty {
+  pub(crate) fn into_inner(self) -> Ty {
     self.0
   }
 }
@@ -83,11 +83,11 @@ impl AsRef<Ty> for Rho {
 /// A [`Ty`] with no [`Ty::ForAll`] at all, i.e. a monotype.
 #[derive(Debug, Clone)]
 #[repr(transparent)]
-pub struct Tau(Ty);
+pub(crate) struct Tau(Ty);
 
 impl Tau {
   /// Returns a new [`Tau`]. Panics in debug mode if it contains [`Ty::ForAll`].
-  pub fn new(ty: Ty) -> Self {
+  pub(crate) fn new(ty: Ty) -> Self {
     debug_assert!(Self::is_valid(&ty));
     Self(ty)
   }
@@ -103,7 +103,7 @@ impl Tau {
   }
 
   /// Unwraps the [`Ty`].
-  pub fn into_inner(self) -> Ty {
+  pub(crate) fn into_inner(self) -> Ty {
     self.0
   }
 }
@@ -130,7 +130,7 @@ pub struct BoundTyVar(Name);
 
 impl BoundTyVar {
   /// Returns a new [`BoundTyVar`].
-  pub fn new(name: Name) -> Self {
+  pub(crate) fn new(name: Name) -> Self {
     Self(name)
   }
 }
@@ -147,7 +147,7 @@ pub struct MetaTyVar(Uniq);
 
 /// Mutable state updated during typechecking.
 #[derive(Debug, Default)]
-pub struct Cx {
+pub(crate) struct Cx {
   uniq_gen: UniqGen,
   skolem_names: FxHashMap<SkolemTyVar, Name>,
   meta_tys: FxHashMap<MetaTyVar, Tau>,
@@ -156,12 +156,12 @@ pub struct Cx {
 
 impl Cx {
   /// Returns a new [`MetaTyVar`] distinct from any other returned thus far.
-  pub fn new_meta_ty_var(&mut self) -> MetaTyVar {
+  pub(crate) fn new_meta_ty_var(&mut self) -> MetaTyVar {
     MetaTyVar(self.uniq_gen.gen())
   }
 
   /// Returns a new [`SkolemTyVar`] distinct from any other returned thus far.
-  pub fn new_skolem_ty_var(&mut self, tv: TyVar) -> SkolemTyVar {
+  pub(crate) fn new_skolem_ty_var(&mut self, tv: TyVar) -> SkolemTyVar {
     let ret = SkolemTyVar(self.uniq_gen.gen());
     let name = self.ty_var_name(tv);
     assert!(self.skolem_names.insert(ret, name).is_none());
@@ -177,23 +177,23 @@ impl Cx {
 
   /// Sets `tv` to refer to `ty`. Panics if `tv` already refers to some other
   /// [`Tau`].
-  pub fn set(&mut self, tv: MetaTyVar, ty: Tau) {
+  pub(crate) fn set(&mut self, tv: MetaTyVar, ty: Tau) {
     assert!(self.meta_tys.insert(tv, ty).is_none());
   }
 
   /// Sets `tv` to refer to `ty`. Panics if `tv` did *not* already refer to some
   /// other [`Tau`].
-  pub fn reset(&mut self, tv: MetaTyVar, ty: Tau) {
+  pub(crate) fn reset(&mut self, tv: MetaTyVar, ty: Tau) {
     assert!(self.meta_tys.insert(tv, ty).is_some());
   }
 
   /// Returns the [`Tau`] that `tv` refers to, if any.
-  pub fn get(&self, tv: MetaTyVar) -> Option<&Tau> {
+  pub(crate) fn get(&self, tv: MetaTyVar) -> Option<&Tau> {
     self.meta_tys.get(&tv)
   }
 
   /// Returns a [`Name`] for the HIR name.
-  pub fn name(&mut self, name: &hir::Name) -> Name {
+  pub(crate) fn name(&mut self, name: &hir::Name) -> Name {
     if let Some(&name) = self.names.get(name) {
       return name;
     }
@@ -205,39 +205,39 @@ impl Cx {
 
 /// Variable names in scope and their types.
 #[derive(Debug, Default, Clone)]
-pub struct Env(FxHashMap<hir::Name, Ty>);
+pub(crate) struct Env(FxHashMap<hir::Name, Ty>);
 
 impl Env {
   /// Insert `name` as having `ty`.
-  pub fn insert(mut self, name: hir::Name, ty: Ty) -> Self {
+  pub(crate) fn insert(mut self, name: hir::Name, ty: Ty) -> Self {
     self.0.insert(name, ty);
     self
   }
 
   /// Returns the [`Ty`] that `name` refers to, if any.
-  pub fn get(&self, name: &hir::Name) -> Option<&Ty> {
+  pub(crate) fn get(&self, name: &hir::Name) -> Option<&Ty> {
     self.0.get(name)
   }
 
   /// Returns an iterator over the values.
-  pub fn values(&self) -> impl Iterator<Item = &Ty> {
+  pub(crate) fn values(&self) -> impl Iterator<Item = &Ty> {
     self.0.values()
   }
 }
 
 /// A slot for a [`Rho`] that starts empty and may be set exactly once.
 #[derive(Debug, Default)]
-pub struct RhoRef(Option<Rho>);
+pub(crate) struct RhoRef(Option<Rho>);
 
 impl RhoRef {
   /// Sets the [`RhoRef`] if its unset. Panics if it is set.
-  pub fn set(&mut self, rho: Rho) {
+  pub(crate) fn set(&mut self, rho: Rho) {
     assert!(self.0.is_none());
     self.0 = Some(rho);
   }
 
   /// Unwraps the [`Rho`] inside, if any. Panics if there is none.
-  pub fn unwrap(self) -> Rho {
+  pub(crate) fn unwrap(self) -> Rho {
     self.0.unwrap()
   }
 }
