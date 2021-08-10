6f11236fedcb03fa1a81ad7c219cbab3cf3e2b0d
Ariel Davis <ariel.z.davis@icloud.com>
2021-08-09 17:41:43 -0700

    Rename

--- crates/tests/src/helpers.rs
+++ crates/tests/src/helpers.rs
@@ -1,10 +1,10 @@
 pub(crate) fn check(s: &str) {
-  let lexed = lex::get(s);
-  assert!(lexed.errors.is_empty());
-  let parsed = parse::get(&lexed.tokens);
-  assert!(parsed.errors.is_empty());
-  let lowered = lower::get(parsed.root);
-  assert_eq!(lowered.top.len(), 1);
-  let expr = *lowered.top.first().unwrap();
-  statics::get(&lowered.arenas, expr);
+  let lex_res = lex::get(s);
+  assert!(lex_res.errors.is_empty());
+  let pars_res = parse::get(&lex_res.tokens);
+  assert!(pars_res.errors.is_empty());
+  let lower_res = lower::get(pars_res.root);
+  assert_eq!(lower_res.top.len(), 1);
+  let expr = *lower_res.top.first().unwrap();
+  statics::get(&lower_res.arenas, expr);
 }
