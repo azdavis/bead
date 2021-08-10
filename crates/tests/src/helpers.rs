a3e7e237a9d949f77f5f6e6e5f32c4f46fed57b2
Ariel Davis <ariel.z.davis@icloud.com>
2021-08-09 17:43:55 -0700

    Improve panics for test helpers

--- crates/tests/src/helpers.rs
+++ crates/tests/src/helpers.rs
@@ -1,10 +1,10 @@
 pub(crate) fn check(s: &str) {
   let lex_res = lex::get(s);
-  assert!(lex_res.errors.is_empty());
+  assert!(lex_res.errors.is_empty(), "lex err");
   let pars_res = parse::get(&lex_res.tokens);
-  assert!(pars_res.errors.is_empty());
+  assert!(pars_res.errors.is_empty(), "parse err");
   let lower_res = lower::get(pars_res.root);
-  assert_eq!(lower_res.top.len(), 1);
-  let expr = *lower_res.top.first().unwrap();
+  assert_eq!(lower_res.top.len(), 1, "bad expr count");
+  let expr = *lower_res.top.first().expect("just checked len() == 1");
   statics::get(&lower_res.arenas, expr);
 }
